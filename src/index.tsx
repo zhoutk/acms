0216daf9cf39a0acf4be4caf152a227f9b7602cd
zhoutk <zhoutk@189.cn>
2019-04-21 12:56:44 +0800

    apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo-hooks';
-import './index.css';
+import { LocaleProvider, message } from 'antd'
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient  } from 'apollo-client'
+import { ApolloLink, NextLink, Observable, Operation } from 'apollo-link';
+import { BatchHttpLink } from 'apollo-link-batch-http';
+import { onError } from 'apollo-link-error';
+import * as React from 'react';
+import { ApolloProvider } from 'react-apollo'
+import * as ReactDOM from 'react-dom';
 import App from './App';
-import apolloClient from './apollo-client'
+import './index.css';
+
 import * as serviceWorker from './serviceWorker';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// 请求拦截器
+const request = async (operation: Operation) => {
+  // 可以设置token
+  operation.setContext({
+    headers: {}
+  })
+  return Promise.resolve()
+}
+
+const requestLink = new ApolloLink((operation: Operation, forward: NextLink) => {
+  return new Observable(observer => {
+    let handle: any;
+    Promise.resolve(operation)
+      .then(oper => request(oper))
+      .then(() => {
+        handle = forward(operation).subscribe({
+          next: observer.next.bind(observer),
+          error: observer.error.bind(observer),
+          complete: observer.complete.bind(observer),
+        });
+      })
+      .catch(observer.error.bind(observer));
+      
+      return () => {
+        if (handle) {
+          handle.unsubscribe()
+        }
+      }
+
+  })
+}) 
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors }) => {
+      // 全局错误处理
+      if (Array.isArray(graphQLErrors)) {
+        message.error(graphQLErrors[0].message)
+      }
+    }),
+    requestLink,
+    new BatchHttpLink({ uri: process.env.REACT_APP_SERVER_URL }),
+  ]),
+  cache: new InMemoryCache(),
+})
+
 ReactDOM.render(
-  <ApolloProvider client={apolloClient}>
-    <App />
-  </ApolloProvider>
-, document.getElementById('root'));
+  <ApolloProvider client={client}>
+    <LocaleProvider locale={zhCN}>
+      <App />
+    </LocaleProvider>
+  </ApolloProvider>,
+  document.getElementById('root') as HTMLElement
+);
 
 serviceWorker.unregister();
